c3cf017cf9ded687643e88871702179b67d93f5f
songjy <songjy@ethicall.cn>
2017-12-21 10:31:56 +0800

    ADD: 初始化数据库

---
+++ service/handler.py
@@ -0,0 +1,80 @@
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
+from model import Hospital, Doctor
+
+client = Elasticsearch('192.168.2.20:9200')
+
+def import_hospital(doc_type):
+    s = Search(using=client, index="hospital-*")
+    s = s.query('match', document_type=doc_type)
+    i = 0
+    for hit in s.scan():
+        i += 1
+        print(hit.hospitalName)
+        print(i)
+        h = Hospital.nodes.get_or_none(hid=hit.document_id)
+        if h: continue
+        data = hit.to_dict()
+        h = Hospital(
+            hid=data['document_id'],
+            name=data['hospitalName'],
+            hType=data.get('hospitalType', None),
+            description=data['description'],
+            managementMode=data.get('managementMode', None),
+            sourceUrl=data.get('source_url', None),
+            sourceType=data.get('document_type', None),
+            province=data.get('province', None),
+            city=data.get('city', None),
+            district=data.get('district', None),
+            street=data.get('street', None),
+            place=data.get('place', None),
+            email=data.get('email', None),
+            direction=data.get('direction', None),
+            website=data.get('website', None),
+            level=data.get('level', None),
+            adcode=data.get('adcode', None),
+            streetNumber=data.get('streetNumber', None),
+            medicalInsurance=data.get('medicalInsurance', None),
+            telephone=data.get('telephone', None),
+        ).save()
+
+def import_doctor(doc_type):
+    s = Search(using=client, index="doctor-*")
+    s = s.query('match', document_type=doc_type)
+#  hits = s.execute()
+    for hit in s.scan():
+        print(hit.name)
+        d = Doctor.nodes.get_or_none(did=hit.document_id)
+        if d: continue
+        data = hit.to_dict()
+        goodat = data.get('goodat', None)
+        description = data.get('description', None)
+        d = Doctor(
+            did=data['document_id'],
+            name=data['name'],
+            goodat="".join(goodat.split()) if goodat else None,
+            province = data.get('province', None),
+            city = data.get('city', None),
+            sex = data.get('sex', None),
+            description = ''.join(description.split()) if description else None,
+            title = data.get('title', None),
+            sourceUrl = data.get('source_url', None),
+            sourceType = data.get('document_type'),
+            headerUrl = data.get('headerUrl', None),
+        ).save()
+        hs = data.get('hospitals', [])
+        deps = []
+        for h in hs:
+            deps.extend(h['departments'])
+            hos = Hospital.nodes.get_or_none(hid=h['hospital_id'])
+            if hos:
+                d.hospital.connect(hos, {
+                    'department': ','.join(h['departments'])
+                })
+        d.department = deps
+        d.save()
+
+
+if __name__ == "__main__":
+    import_hospital('guahaowang')
+    import_doctor('guahaowang')
---
+++ service/model.py
@@ -0,0 +1,74 @@
+from neomodel import config
+from neomodel import (config, StructuredNode, StringProperty, IntegerProperty,
+    UniqueIdProperty, RelationshipTo, RelationshipFrom, FloatProperty,
+                      StructuredRel, ArrayProperty)
+
+config.DATABASE_URL = 'bolt://neo4j:admin@neo4j:7687'
+
+class HospitalSimilarity(StructuredRel):
+    similarity = FloatProperty()
+    coordDistance = IntegerProperty()
+    province = StringProperty()
+    city = StringProperty()
+    district = StringProperty()
+
+class Hospital(StructuredNode):
+    SOURCETYPE = (
+        ('qqyy', "中国医院大全"),
+        ('zhizhuwang', "蜘蛛网"),
+        ('cnkang', "中华康网"),
+        ('yyk99', "99健康网"),
+        ('yixuebaike', "医学百科"),
+        ('xywy', "寻医问药"),
+        ('xsjk', "携手健康网"),
+        ('familydoctor', "家庭医生在线"),
+        ('mapbar', "图吧"),
+        ('haodf', "好大夫在线"),
+        ('guahaowang', "挂号网"),
+    )
+    hid = UniqueIdProperty()
+    name = StringProperty()
+    hType = StringProperty()
+    description = StringProperty()
+    managementMode = StringProperty()
+    sourceUrl = StringProperty()
+    sourceType = StringProperty(choices=SOURCETYPE)
+    province = StringProperty()
+    city = StringProperty()
+    district = StringProperty()
+    street = StringProperty()
+    place = StringProperty()
+    email = StringProperty()
+    direction = StringProperty()
+    website = StringProperty()
+    level = StringProperty()
+    adcode = StringProperty()
+    streetNumber = StringProperty()
+    medicalInsurance = StringProperty()
+    telephone = StringProperty()
+    longitude = FloatProperty()
+    latitude = FloatProperty()
+    aimilarity = RelationShipTo(Hospital, "HOSPITALSIMILARITY",
+                                model=HospitalSimilarity)
+
+class HosDocRel(StructuredRel):
+    department = StringProperty()
+
+class DoctorSimilarity(StructuredRel):
+    similarity = FloatProperty()
+    department = StringProperty()
+
+class Doctor(StructuredNode):
+    did = UniqueIdProperty()
+    name = StringProperty()
+    goodat = StringProperty()
+    province = StringProperty()
+    city = StringProperty()
+    sex = StringProperty()
+    description = StringProperty()
+    title = StringProperty()
+    sourceUrl = StringProperty()
+    sourceType = StringProperty()
+    headerUrl = StringProperty()
+    department = ArrayProperty()
+    hospital = RelationshipTo(Hospital, 'WORKIN', model=HosDocRel)
