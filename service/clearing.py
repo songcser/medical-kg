17e6c847e843e763a4389a12b142f1891eed24dc
songjy <songjy@ethicall.cn>
2018-01-10 16:47:24 +0800

    ADD: check hospital name

--- service/clearing.py
+++ service/clearing.py
@@ -3,7 +3,17 @@ import re
 
 from model import Hospital, SOURCETYPE
 
-handlers = ['name', 'coordinate']
+handlers = ['check', 'name', 'coordinate']
+
+def checkHospitalName():
+    pattern = re.compile(r"[\s+\.\!\/_,$%^*(+\"\')]+|[+——()?【】“”！，。？、~@#￥%……&*（）]+'")
+    for docs in SOURCETYPE:
+        for hos in Hospital.nodes.filter(sourceType=docs[0]):
+            name = hos.fullName
+            nss = pattern.findall(name)
+            if nss:
+                print(name)
+
 
 def cleanHopsitalName():
     #  pattern = re.compile(r"[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）]+")
@@ -16,8 +26,6 @@ def cleanHopsitalName():
             print("%s-%s" % (hos.sourceType, name))
             ns = pattern.sub("", name)
             nss = pattern.findall(name)
-            print(ns)
-            print(nss)
             hos.name = ns
             if nss:
                 hos.nickName = ",".join([ls for lss in nss for ls in lss if ls])
@@ -30,8 +38,9 @@ def cleanHospitalCoordinate():
 @click.option('--handler', '-h', type=click.Choice(handlers), multiple=True)
 def cleanHospital(handler):
     hs = handler if handler else handlers
+    if 'check' in hs:
+        cleckHospitalName()
     if 'name' in hs:
         cleanHopsitalName()
     if 'coordinate' in hs:
         cleanHospitalCoordinate()
-
